7165f7287f6e0e537d189c1e9137044f976987f0
Ana María Guzmán Hoyos <anamariaguzman1996@hotmail.com>
2021-11-18 20:01:59 -0500

    Agregro Homework totalmente resuelta y captura del npm test aprobado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -244,11 +244,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra==['a', 'e', 'i', 'o', 'u']){
+  if(letra==='a' || letra==='e' || letra==='i' || letra==='o' || letra==='u'){
     return('Es vocal');
-  }else if (letra.length>1){
+  }else if (letra.length!==1){
     return('Dato incorrecto')
-  }else if (letra!=['a', 'e', 'i', 'o', 'u']){
+  }else if (letra!=='a' || letra!=='e' || letra!=='i' || letra!=='o' || letra!=='u'){
     return('Dato incorrecto') //33
   }
 }
